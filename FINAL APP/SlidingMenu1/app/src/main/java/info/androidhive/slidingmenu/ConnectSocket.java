510236c38328b224bfc9e63c2093cf2e227129a4
JohannesUcel <u.johannes@hotmail.com>
2015-05-30 22:25:15 +0200

    Connect Socket

---
+++ FINAL APP/SlidingMenu1/app/src/main/java/info/androidhive/slidingmenu/ConnectSocket.java
@@ -0,0 +1,23 @@
+package info.androidhive.slidingmenu;
+import java.io.IOException;
+import java.io.*;
+import java.net.*;
+/**
+ * Created by Johannes on 30.05.2015.
+ */
+public class ConnectSocket {
+    public void connect(String cmd) throws IOException{
+        InetAddress address = InetAddress.getByName("10.0.0.1");
+        Socket socket = new Socket(address, 50000);
+
+        BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+
+        OutputStreamWriter osw = new OutputStreamWriter(bos, "US-ASCII");
+
+        System.out.println("Sending message...");
+
+        osw.write(cmd   );
+        osw.flush();
+        osw.close();
+    }
+}
